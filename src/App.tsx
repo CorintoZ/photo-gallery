3592bc7ef424d988818440457038c854cb3e9b34
CorintoZ <david.moreno92@gmail.com>
2023-11-30 11:25:01 +0100

    Display layout selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { styled } from 'styled-components';
 import './App.css';
+import { LayoutSwitch } from './components/LayoutSwitch/LayoutSwitch';
+import { useGlobalContext } from './context/store';
+
+const Container = styled.div`
+  margin: 2rem;
+`;
 
 function App() {
+  const { state } = useGlobalContext();
+  const { selectedLayout, photos } = state;
+
   return (
-  <div>Welcome</div>
+    <Container>
+      <LayoutSwitch />
+      {selectedLayout === 'cards' && <div>cards</div>}
+      {selectedLayout === 'grid' && <div>grid</div>}
+      {selectedLayout === 'list' && <div>list</div>}
+      {selectedLayout === 'carousel' && <div>carousel</div>}
+    </Container>
   );
 }
 
