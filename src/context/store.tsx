05f3ea4ce4db446f6e5d52d0feb527185f7b2944
CorintoZ <david.moreno92@gmail.com>
2023-11-30 11:05:51 +0100

    Create globalContext wrapper

---
+++ src/context/store.tsx
@@ -0,0 +1,54 @@
+import React, { createContext, ReactNode, useContext, useEffect, useReducer } from 'react';
+import { TPhoto, usePhotos } from '../hooks/usePhotos';
+
+type GlobalState = {
+  photos: TPhoto[];
+  selectedLayout: string;
+}
+
+type GlobalProviderProps = {
+  children: ReactNode;
+}
+
+export type Action =
+  | { type: 'SET_PHOTOS'; payload: TPhoto[] }
+  | { type: 'SET_SELECTED_LAYOUT'; payload: string };
+
+const initialState: GlobalState = {
+  photos: [],
+  selectedLayout: 'cards',
+};
+
+const reducer = (state: GlobalState, action: Action): GlobalState => {
+  switch (action.type) {
+    case 'SET_PHOTOS':
+      return { ...state, photos: action.payload };
+    case 'SET_SELECTED_LAYOUT':
+      return { ...state, selectedLayout: action.payload };
+    default:
+      return state;
+  }
+};
+
+const GlobalContext = createContext<{ state: GlobalState; dispatch: React.Dispatch<Action> } | undefined>(
+  undefined
+);
+
+export const GlobalProvider: React.FC<GlobalProviderProps> = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const photos = usePhotos();
+
+  useEffect(() => {
+    dispatch({ type: 'SET_PHOTOS', payload: photos });
+  }, [photos]);
+
+  return <GlobalContext.Provider value={{ state, dispatch }}>{children}</GlobalContext.Provider>;
+};
+
+export const useGlobalContext = () => {
+  const context = useContext(GlobalContext);
+  if (!context) {
+    throw new Error('useGlobalContext must be used within a GlobalProvider');
+  }
+  return context;
+};
